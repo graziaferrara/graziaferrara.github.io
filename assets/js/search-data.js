9f99e7a05268d73115ed9103d24c990db59ba1c7
graziaferrara <grferrara@unisa.it>
2026-07-08 17:13:24 +0000

    Deploying to gh-pages from @ graziaferrara/graziaferrara.github.io@d58dd0275c6b97ce3fc56fa978fddac7e0eceae4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -68,6 +68,12 @@ ninja.data = [{
           section: "News",},{id: "news-presented-our-paper-influence-maximization-in-unknown-social-networks-a-contextual-bandit-approach-extended-abstract-at-prima-2025-in-modena-italy",
           title: 'Presented our paper “Influence Maximization in Unknown Social Networks: A Contextual Bandit Approach...',
           description: "",
+          section: "News",},{id: "news-presented-an-extended-version-of-our-paper-influence-maximization-in-unknown-social-networks-a-contextual-bandit-approach-at-the-asi-workshop-aamas-2026-also-attended-aamas-in-paphos-cyprus",
+          title: 'Presented an extended version of our paper “Influence Maximization in Unknown Social Networks:...',
+          description: "",
+          section: "News",},{id: "news-volunteering-at-ec-2026-in-rome-italy",
+          title: 'Volunteering at EC 2026 in Rome, Italy.',
+          description: "",
           section: "News",},{
         id: 'social-email',
         title: 'email',
